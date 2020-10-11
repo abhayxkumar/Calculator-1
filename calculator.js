d35a37ba547740634e5d5c76224494bc2b65ee00
Anshkulshreshtha <70211189+Anshkulshreshtha@users.noreply.github.com>
2020-10-11 12:11:06 +0530

    add files via upload

---
+++ calculator.js
@@ -0,0 +1,37 @@
+//jshint esversions:6
+
+const express = require("express");
+const bodyparser = require("body-parser");
+
+const app = express();
+app.use(bodyparser.urlencoded({extended: true}));
+
+app.get("/", function(require, respond){
+  respond.sendfile(__dirname + "/index.html");
+});
+
+app.post("/index.html",  function(request,respond){
+
+  var num1 = Number(request.body.n1);
+  var num2 = Number(request.body.n2);
+
+  var result = num1 + num2;
+  respond.send("The result of calculation is " + result);
+});
+
+app.get("/bmicalculator", function(request,respond){
+  respond.sendfile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(request, respond){
+  var weight = parseFloat(request.body.weight);
+  var height = parseFloat(request.body.height);
+
+  var bmi = weight / (height * height);
+  respond.send("your BMI is " + bmi);
+
+});
+
+app.listen(3000, function(){
+  console.log("server started on port 3000");
+});
